381b7352a7b59f2ec4f7424fd51300eabb93ea15
Illia Medvediev <bearillia@gmail.com>
2023-12-21 20:11:04 +0200

    Fix: Fixed losing 'this' context when mapping over the values array

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -8,11 +8,13 @@ export interface IUtilsService {
 export class UtilsService implements IUtilsService {
   public stripNullValues(value: unknown): unknown {
     if (Array.isArray(value)) {
-      return value.map(this.stripNullValues);
+      return value.map(this.stripNullValues.bind(this));
     }
+
     if (value !== null && typeof value === 'object') {
       return Object.fromEntries(Object.entries(value).map(([key, value]) => [key, this.stripNullValues(value)]));
     }
+
     if (value !== null) {
       return value;
     }
